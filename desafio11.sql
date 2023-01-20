01a69b9ba81e1c0129082dff6cafa764888e6fdf
Gabriel Galera <gabgalera@hotmail.com>
2023-01-20 16:20:37 -0300

    Tenta arrumar desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,12 @@
 SELECT notes FROM purchase_orders
+WHERE 
+	notes LIKE '%30' OR 
+    notes LIKE '%31' OR 
+    notes LIKE '%32' OR 
+    notes LIKE '%33' OR
+    notes LIKE '%34' OR 
+    notes LIKE '%35' OR 
+    notes LIKE '%36' OR 
+    notes LIKE '%37' OR 
+    notes LIKE '%38' OR 
+    notes LIKE '%39' OR 
